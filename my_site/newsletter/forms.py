a827f41022590d80f6e68cda61c23e372226cf9a
Nitin Sai <musicssns@gmail.com>
2021-01-18 19:37:00 +0530

    changed ui

--- my_site/newsletter/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class SubscriberForm(forms.Form):
-    email = forms.EmailField(label='Your email',
-                             max_length=100,
-                             widget=forms.EmailInput(attrs={'class': 'form-control'}))
