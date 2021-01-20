13b4e6a04ee97cdc66f1e59b39bdfb859ab81f85
Nitin Sai <musicssns@gmail.com>
2021-01-20 19:52:12 +0530

    .

---
+++ my_site/checker/profanity_check.py
@@ -0,0 +1,18 @@
+import numpy as np
+import joblib
+import os, sys
+
+os.chdir('checker')
+print(os.getcwd())
+
+model = joblib.load('data/model.joblib')
+vectorizer = joblib.load('data/vectorizer.joblib')
+
+def _get_profane_prob(prob):
+  return prob[1]
+
+def predict(texts):
+  return model.predict(vectorizer.transform(texts))
+
+def predict_prob(texts):
+  return np.apply_along_axis(_get_profane_prob, 1, model.predict_proba(vectorizer.transform(texts)))
